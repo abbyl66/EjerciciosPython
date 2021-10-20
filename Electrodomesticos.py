ca6872aacd2158f64a2fa50707e6b6c705bb96c2
Abby <91982188+abbyl66@users.noreply.github.com>
2021-10-20 19:12:13 +0200

    Update Electrodomesticos.py

--- Electrodomesticos.py
+++ Electrodomesticos.py
@@ -1,6 +1,3 @@
-import Lavadora
-
-
 class Electrodomesticos:
 
     #Constructor
@@ -14,10 +11,13 @@ class Electrodomesticos:
 
     def getpvbase (self):
         return self.__pvbase
+
     def getcolor (self):
         return self.__color
+
     def getconsEnerg (self):
         return self.__consEnerg
+
     def getpeso (self):
         return self.__peso
 
@@ -62,14 +62,67 @@ class Electrodomesticos:
         elif int(self.__peso) >= 80:
             self.__pvbase = int(self.__pvbase) + 100
 
+
+#CLASE LAVADORA
+class Lavadora(Electrodomesticos):
+
+    def __init__(self, pvbase, color, consEnerg, peso, carga=5):
+       Electrodomesticos.__init__(self, pvbase, color, consEnerg, peso)
+       self.__carga = carga
+
+    def getcarga (self):
+        return self.__carga
+
+    def precioFinal(self):
+        if self.__carga > 30:
+            super().__pvbase = super().__pvbase+50
+
+    def __str__(self):
+        return super().__str__()+" | Carga : " +str(self.__carga)
+
+
+#CLASE TELEVISION
+class Television(Electrodomesticos):
+
+    def __init__(self, pvbase, color, consEnerg, peso, res=20, fourK=False):
+        Electrodomesticos.__init__(self, pvbase, color, consEnerg, peso)
+        self.__res = res
+        self.__fourK = fourK
+
+    def getres(self):
+        return self.__res
+    def getfourK (self):
+        return self.getres()
+
+    def precioFin(self):
+        if self.__res > 40:
+            super().__pvbase = int(super().__pvbase) * 1.3
+
+        if self.__fourK == True:
+            super().__pvbase = int(super().__pvbase) +50
+
+    def __str__(self):
+        return super().__str__()+ " | Resolución: " +str(self.__res)+" | FourK: " +str(self.__fourK)
+
 if __name__ == "__main__":
 
     lista_elect = ()
     lista_elect =list(lista_elect)
 
-    lista_elect.insert(0, Electrodomesticos(pvbase=input("Introduzca el precio: "), color=input("Introduzca el color: "),
-                           consEnerg=input("Introduzca el consumo: "), peso=input("Introduzca el peso: ")))
-    lista_elect.insert(1, Electrodomesticos(pvbase=input("Introduzca el precio: "), color=input("Introduzca el color: "),
-                           consEnerg=input("Introduzca el consumo: "), peso=input("Introduzca el peso: ")))
+    lista_elect.insert(0, Electrodomesticos(pvbase=60, color="Rosa", consEnerg="A", peso=12))
+    lista_elect.insert(1, Lavadora(pvbase=90, color="Gris", consEnerg="F", peso=15, carga=10))
+    lista_elect.insert(2, Television(pvbase=50, color="Azul", consEnerg="B", peso=16, res=30, fourK=True))
+
+    for x in range (0, len(lista_elect)):
+        Electrodomesticos.comprobarcolor(lista_elect[x])
+        Electrodomesticos.comprobarComsumoE(lista_elect[x])
+        Electrodomesticos.precioFinalc(lista_elect[x])
+        Electrodomesticos.precioFinalp(lista_elect[x])
+    Lavadora.precioFinal(lista_elect[1])
+
+
+    #Recorrer lista
+    for x in range(0,len(lista_elect)):
+        print(lista_elect[x])
 
 
